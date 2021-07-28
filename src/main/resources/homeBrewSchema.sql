c965090d016e5aa6657a1fcd0fea48fa666c8d13
daryl-bishop <86105232+daryl-bishop@users.noreply.github.com>
2021-07-28 09:59:20 +0100

    Revert "Crud merge"

--- src/main/resources/homeBrewSchema.sql
+++
@@ -1,3 +0,0 @@
-drop table if exists homebrew CASCADE; 
-create table homebrew (id integer primary key AUTO_INCREMENT, 
-type varchar(255), name varchar(255), percentage integer not null, brew_time integer not null);
